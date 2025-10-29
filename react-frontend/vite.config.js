5645ce2c87ee3f5c93e16267f4a5b10591cc3f26
Gerald <Gerald@LAPTOP-4GEP9M9H>
2025-10-29 08:55:24 +0800

    feat: add PWA plugin and configuration

--- react-frontend/vite.config.js
+++ react-frontend/vite.config.js
@@ -1,7 +1,61 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+import { VitePWA } from 'vite-plugin-pwa';
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    VitePWA({
+    registerType: "autoUpdate",
+    includeAssets: ["vite.svg"],
+    manifest: {
+      name: "My React App",
+      short_name: "ReactApp",
+      description: "My Awesome React Application",
+      theme_color: "#ffffff",
+      icons: [
+        {
+          src: "favicon.ico",
+          sizes: "64x64 32x32 24x24 16x16",
+          type: "image/x-icon",
+        },
+        {
+          src: "logo192.png",
+          type: "image/png",
+          sizes: "192x192",
+        },
+        {
+          src: "logo512.png",
+          type: "image/png",
+          sizes: "512x512",
+        },
+      ],
+      start_url: "/index.html",
+      display: "standalone",
+      background_color: "#333333",
+    },
+    workbox: {
+      globPatterns: ["**/*.{js,css,html,png,svg,ico,json}"],
+      navigateFallback: "/index.html",
+      runtimeCaching: [
+        {
+          urlPattern: ({ request }) =>
+            request.destination === "document" ||
+            request.destination === "script" ||
+            request.destination === "style" ||
+            request.destination === "image" ||
+            request.destination === "font",
+          handler: "NetworkFirst",
+          options: {
+            cacheName: "offline-cache",
+            expiration: { maxEntries: 50, maxAgeSeconds: 86400 },
+          },
+        },
+      ],
+    },
+    devOptions: { enabled: true },
+  }),
+  ],
 })
